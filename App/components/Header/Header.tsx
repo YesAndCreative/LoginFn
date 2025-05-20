83c279ddbe435ebf8ac50209001edadbaa8bf0c7
ZHOON <zhoon95@gmail.com>
2025-05-21 08:31:53 +0900

    COMMIT

--- App/components/Header/Header.tsx
+++ App/components/Header/Header.tsx
@@ -6,7 +6,10 @@ const Header = () => {
     <header className="fixed top-0 z-[1000] w-full h-[50px] min-h-[50px] bg-black border pointer-events-none">
       <Wrapper className="flex h-full items-center justify-between">
         <div className="LOGO text-white">Logo</div>
-        <div className="NAVIGATION/SIGNUP text-white">Navigation</div>
+        <div className="NAVIGATION/SIGNUP text-white flex items-center gap-4">
+          <div>Navigation</div>
+          <div>Signup</div>
+        </div>
       </Wrapper>
     </header>
   );
