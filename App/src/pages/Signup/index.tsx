fded3958157a49c61a7d7d4a9eaa3ef369d36d30
ZHOON <zhoon95@gmail.com>
2025-05-22 16:58:36 +0900

    Feat : Setting for Form

--- App/src/pages/Signup/index.tsx
+++ App/src/pages/Signup/index.tsx
@@ -1,7 +1,39 @@
 import { useState, useEffect } from "react";
 import useSWRMutation from "swr/mutation";
 import axios from "axios";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import Wrapper from "@/components/Wrapper";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(2, { message: "Username must be at least 2 characters." })
+    .max(50, { message: "Username must be less than 50 characters." }),
+});
+
+const form = useForm<z.infer<typeof formSchema>>({
+  resolver: zodResolver(formSchema),
+  defaultValues: {
+    username: "",
+  },
+});
+
+const onSubmit = (values: z.infer<typeof formSchema>) => {
+  console.log(values);
+};
 
 const Signup = () => {
   return (
